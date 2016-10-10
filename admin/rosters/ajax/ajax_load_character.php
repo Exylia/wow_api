54edcbcb8bdc62bd9fd1c99cb82753e64d0ac899
Sylvain Bouchet <sylvain.bouchet@globalis-ms.com>
2016-10-10 14:02:58 +0200

    Ajout ajax de chargement d'un personnage

---
+++ admin/rosters/ajax/ajax_load_character.php
@@ -0,0 +1,108 @@
+<?php
+
+require_once 'start_php.php';
+
+// Réponse au format JSON
+header('Content-Type: application/json; charset=UTF-8');
+
+// S'il manque des arguments
+if (empty($_POST) || empty($_POST['realm']) || empty($_POST['name'])) {
+    header('HTTP/1.1 422 Missing arguments');
+    die(json_encode(array('error' => 'Missing arguments')));
+}
+
+require_once 'lib/wow_api/wow_api_loader.php';
+
+// Recuperation des informations du personnage
+$character = (array) getCharacterAllInformations($_POST['realm'], $_POST['name']);
+
+//
+// Error personnage introuvable a gerer ici !
+//
+
+// Test si le personnage existe deja en base
+$sql = 'SELECT * FROM `' . CFG_TABLE_CHARACTER . '` ';
+$sql.= 'WHERE ';
+$sql.= 'realm = ' . $pdo->quote($_POST['realm']) . ' AND ';
+$sql.= 'name  = ' . $pdo->quote($_POST['name']);
+
+$result = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+$character_id = '';
+
+// existe deja dans la table character-> mise à jour
+if ($result) {
+
+    $character_id = $result['character_id'];
+
+    $sql = 'UPDATE `' . CFG_TABLE_CHARACTER . '` SET ';
+    $sql.= 'name    = \'' . $character['name']    . '\', ';
+    $sql.= 'realm   = \'' . $character['realm']   . '\', ';
+    $sql.= 'faction = \'' . $character['faction'] . '\', ';
+    $sql.= 'class   = \'' . $character['class']   . '\', ';
+    $sql.= 'race    = \'' . $character['race']    . '\', ';
+    $sql.= 'gender  = \'' . $character['gender']  . '\', ';
+    $sql.= 'level   = \'' . $character['level']   . '\', ';
+    $sql.= 'region  = \'EU\' ';
+    $sql.= 'WHERE ';
+    $sql.= 'character_id = ' . $pdo->quote($character_id);
+
+    $pdo->query($sql);
+
+} else {    // Sinon Insertion
+
+    $sql = 'INSERT INTO `' . CFG_TABLE_CHARACTER . '` SET ';
+    $sql.= 'name    = \'' . $character['name']    . '\', ';
+    $sql.= 'realm   = \'' . $character['realm']   . '\', ';
+    $sql.= 'faction = \'' . $character['faction'] . '\', ';
+    $sql.= 'class   = \'' . $character['class']   . '\', ';
+    $sql.= 'race    = \'' . $character['race']    . '\', ';
+    $sql.= 'gender  = \'' . $character['gender']  . '\', ';
+    $sql.= 'level   = \'' . $character['level']   . '\', ';
+    $sql.= 'region  = \'EU\'';
+
+    $pdo->query($sql);
+
+    // Recuperation de l'id
+    $character_id = $pdo->lastInsertId();
+
+}
+
+// Test si les stats du perso sont deja presente en base
+$character_stats = $character['stats'];
+
+$sql = 'SELECT * FROM `' . CFG_TABLE_CHARACTER_STATS . '` ';
+$sql.= 'WHERE ';
+$sql.= 'character_id = ' . $pdo->quote($character_id);
+
+$result = $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+
+var_dump($result);
+
+// Stat deja presente -> mise à jour
+if ($result) {
+
+    $sql = 'UPDATE ' . CFG_TABLE_CHARACTER_STATS . ' SET ';
+
+    foreach ($character_stats as $key => $value) {
+        $sql.= '`' . $key . '` = \'' . $value . '\', ';
+    }
+
+    $sql = substr($sql, 0, -2);
+
+    $pdo->query($sql);
+
+} else {    // Sinon insertion
+
+    $sql = 'INSERT INTO ' . CFG_TABLE_CHARACTER_STATS . ' SET ';
+    $sql.= 'character_id = ' . $pdo->quote($character_id) . ', ';
+
+    foreach ($character_stats as $key => $value) {
+        $sql.= '`' . $key . '` = \'' . $value . '\', ';
+    }
+
+    $sql = substr($sql, 0, -2);
+
+    $pdo->query($sql);
+}
+?>
