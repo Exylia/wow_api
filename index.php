5f28b758f180545396eb69f5eb45bfdb60603af1
Sylvain Bouchet <sylvain.bouchet@globalis-ms.com>
2016-10-24 14:00:16 +0200

    Formulaire de recherche de personnage sur page d'accueil

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
-<?php 
+<?php
 
 require_once 'start_php.php';
+
+if (!empty($_GET)) {
+    if (empty($_GET['realm'])) {
+        $error['realm'] = "Serveur : champ obligatoire";
+    }
+
+    if (empty($_GET['name'])) {
+        $error['name'] = "Nom : champ obligatoire";
+    }
+
+    if (empty($error)) {
+        // Recuperation des informations du personnage
+        $character = (array) getCharacterAllInformations($_GET['realm'], $_GET['name']);
+
+        if (!empty($character['status']) && $character['status'] === 'nok') {
+            $error['all'] = "Personnage introuvable";
+        }
+    }
+}
 
 require_once 'stop_php.php';
 
